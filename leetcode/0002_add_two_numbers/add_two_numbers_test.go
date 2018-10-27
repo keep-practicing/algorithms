2c6e94a9dac5d08514fcc45b01c0dc094383f6e2
zwfang <zwfangio@hotmail.com>
2018-10-27 21:52:21 +0800

    add two numbers

---
+++ leetcode/0002_add_two_numbers/add_two_numbers_test.go
@@ -0,0 +1,40 @@
+package add_two_numbers
+
+import "testing"
+
+func TestAddTwoNumbers1(t *testing.T) {
+	var (
+		l1Vals   = [3]int{2, 4, 3}
+		l2Vals   = [3]int{5, 6, 4}
+		l1       = ListNode{Val: -1}
+		l2       = l1
+		cur1     = &l1
+		cur2     = &l2
+		addLi1   [3]int
+		testFunc = [2]func(l1 *ListNode, l2 *ListNode) *ListNode{addTwoNumbers1, addTwoNumbers2}
+	)
+
+	expectedLi := [3]int{7, 0, 8}
+
+	for i := 0; i < len(l1Vals); i++ {
+		cur1.Next = &ListNode{Val: l1Vals[i]}
+		cur1 = cur1.Next
+	}
+	for i := 0; i < len(l2Vals); i++ {
+		cur2.Next = &ListNode{Val: l2Vals[i]}
+		cur2 = cur2.Next
+	}
+
+	for i := 0; i < 2; i++ {
+		addHead := testFunc[i](l1.Next, l2.Next)
+
+		for i := 0; addHead != nil; i++ {
+			addLi1[i] = addHead.Val
+			addHead = addHead.Next
+		}
+
+		if addLi1 != expectedLi {
+			t.Error("测试不通过")
+		}
+	}
+}
--- leetcode/README.md
+++ leetcode/README.md
@@ -2,6 +2,7 @@
 
 |题号|题目|solution|难度|
 |:---:|:---:|:---:|:---:|
+|0002|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)|[solution](0002_add_two_numbers/README.md)|Medium|
 |0021|[Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/)|[solution](0021_merge_two_sorted_lists/README.md)|Easy|
 |0025|[Reverse Nodes in k-Group](https://leetcode.com/problems/reverse-nodes-in-k-group/)|[solution](./0025_reverse_nodes_in_k_group/README.md)|Hard|
 |0061|[Rotate List](https://leetcode.com/problems/rotate-list/)|[solution](./0061_rotate_list/README.md)|Medium|
