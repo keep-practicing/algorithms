48aca93d8b149dda6a3b21e1cd1424d04cbb38a4
Fang, Zhaowei <33212232+zwfang@users.noreply.github.com>
2018-10-28 21:52:33 +0800

    single linked list reverse (#5)
    * single linked list reverse
    
    * add explain picture
    
    * optimize code
    
    * optmize code
    
    * solution 2 for single linked list reverse
    
    * fix test bug
    
    * add solution 4 for single linked list reverse (recursion)
    
    * update essay/readme.md

---
+++ essay/linked_list/single_linked_list/single_linked_list_reverse.go
@@ -0,0 +1,58 @@
+package singleLinkedList
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 方法二
+func reverseSingleLinkedList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var (
+		q = head.Next
+		r *ListNode
+	)
+
+	head.Next = nil
+	for q != nil {
+		r = q.Next
+		q.Next = head
+		head = q
+		q = r
+	}
+	return head
+}
+
+// 方法三
+func reverseSingleLinkedList2(head *ListNode) *ListNode {
+	var (
+		p = head.Next
+		q *ListNode
+	)
+
+	for p.Next != nil {
+		q = p.Next
+		p.Next = q.Next
+		q.Next = head.Next
+		head.Next = q
+	}
+
+	p.Next = head      // 单链表成环
+	head = p.Next.Next // 新head变为原head的next  head=q
+	p.Next.Next = nil  // 断链表
+	return head
+}
+
+// 方法四 递归
+func reverseSingleLinkedList3(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head //链表为空直接返回，head.Next 为空时递归基
+	}
+
+	newHead := reverseSingleLinkedList3(head.Next) // 递归调用
+	head.Next.Next = head                          // 分析两个节点的情况，形成环
+	head.Next = nil                                // 断开链表
+	return newHead
+}
