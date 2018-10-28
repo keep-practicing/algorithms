2ec10673c3ab4bd928720db1705778d9cdcff87e
Fang, Zhaowei <33212232+zwfang@users.noreply.github.com>
2018-10-28 08:49:32 +0800

    add two numbers (#4)
    * add two numbers
    
    * don't use an underscore in package name
    
    * add more test case for add two numbers
    
    * add test case for add two numbers
    
    * add test case for add two numbers

---
+++ leetcode/0002_add_two_numbers/add_two_numbers.go
@@ -0,0 +1,80 @@
+package addTwoNumbers
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 解法一，暴力解法
+func addTwoNumbers1(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := ListNode{Val: -1}
+	cur := &head
+	carry := 0
+	for l1 != nil && l2 != nil {
+		sum := l1.Val + l2.Val + carry
+		val := sum % 10
+		carry = sum / 10
+		node := ListNode{Val: val}
+		cur.Next = &node
+		cur = cur.Next
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+	if carry == 0 {
+		if l1 != nil {
+			cur.Next = l1
+		}
+		if l2 != nil {
+			cur.Next = l2
+		}
+		return head.Next
+	} else {
+		for l1 != nil {
+			sum := l1.Val + carry
+			val := sum % 10
+			carry = sum / 10
+			node := ListNode{Val: val}
+			cur.Next = &node
+			cur = cur.Next
+			l1 = l1.Next
+		}
+		for l2 != nil {
+			sum := l2.Val + carry
+			val := sum % 10
+			carry = sum / 10
+			node := ListNode{Val: val}
+			cur.Next = &node
+			cur = cur.Next
+			l2 = l2.Next
+		}
+		for carry != 0 {
+			val := carry % 10
+			carry = carry / 10
+			node := ListNode{Val: val}
+			cur.Next = &node
+			cur = cur.Next
+		}
+	}
+	return head.Next
+}
+
+// 解法二，递归
+func addTwoNumbers2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	} else if l1 == nil && l2 != nil {
+		return l2
+	} else if l1 != nil && l2 == nil {
+		return l1
+	}
+
+	sum := l1.Val + l2.Val
+	next := addTwoNumbers2(l1.Next, l2.Next)
+
+	if sum >= 10 {
+		carry := sum / 10
+		sum %= 10
+		next = addTwoNumbers2(next, &ListNode{Val: carry})
+	}
+	return &ListNode{Val: sum, Next: next}
+}
---
+++ leetcode/0002_add_two_numbers/add_two_numbers_test.go
@@ -0,0 +1,66 @@
+package addTwoNumbers
+
+import "testing"
+
+func TestAddTwoNumbers1(t *testing.T) {
+	var (
+		liCase   map[string][]int
+		liCases  []map[string][]int
+		testFunc = [2]func(l1 *ListNode, l2 *ListNode) *ListNode{addTwoNumbers1, addTwoNumbers2}
+	)
+
+	// case 1
+	liCase = make(map[string][]int)
+	liCase["l1"] = []int{2, 4, 3}
+	liCase["l2"] = []int{5, 6, 4}
+	liCase["expected"] = []int{7, 0, 8}
+	liCases = append(liCases, liCase)
+
+	// case 2
+	liCase["l1"] = []int{5}
+	liCase["l2"] = []int{5}
+	liCase["expected"] = []int{0, 1}
+	liCases = append(liCases, liCase)
+
+	// case 3
+	liCase["l1"] = []int{1}
+	liCase["l2"] = []int{9, 9, 9}
+	liCase["expected"] = []int{0, 0, 0, 1}
+	liCases = append(liCases, liCase)
+
+	// case 4
+	liCase["l1"] = []int{9, 9}
+	liCase["l2"] = []int{1}
+	liCase["expected"] = []int{0, 0, 1}
+	liCases = append(liCases, liCase)
+
+	// case 5
+	liCase["l1"] = []int{7}
+	liCase["l2"] = []int{8, 9}
+	liCase["expected"] = []int{5, 0, 1}
+	liCases = append(liCases, liCase)
+
+	for i := 0; i < len(liCases); i++ {
+		l1 := sliToList(liCases[i]["l1"])
+		l2 := sliToList(liCases[i]["l2"])
+		for j := 0; j < len(testFunc); j++ {
+			newL := testFunc[j](l1, l2)
+			for k := 0; k < len(liCases[i]["expected"]); k++ {
+				if liCases[i]["expected"][k] != newL.Val {
+					t.Error("测试不通过")
+				}
+				newL = newL.Next
+			}
+		}
+	}
+}
+
+func sliToList(sli []int) *ListNode {
+	li := ListNode{Val: -1}
+	cur := &li
+	for i := 0; i < len(sli); i++ {
+		cur.Next = &ListNode{Val: sli[i]}
+		cur = cur.Next
+	}
+	return li.Next
+}
