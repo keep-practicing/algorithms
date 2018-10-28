63da1d67992dc0a22fb66287b50f2c612106ed31
zwfang <zwfangio@hotmail.com>
2018-10-28 20:55:51 +0800

    add solution 4 for single linked list reverse (recursion)

--- essay/linked_list/single_linked_list/single_linked_list_reverse.go
+++ essay/linked_list/single_linked_list/single_linked_list_reverse.go
@@ -44,3 +44,15 @@ func reverseSingleLinkedList2(head *ListNode) *ListNode {
 	p.Next.Next = nil  // 断链表
 	return head
 }
+
+// 方法四 递归
+func reverseSingleLinkedList3(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head //链表为空直接返回，head.Next 为空时递归基
+	}
+
+	newHead := reverseSingleLinkedList3(head.Next) // 递归调用
+	head.Next.Next = head                          // 分析两个节点的情况，形成环
+	head.Next = nil                                // 断开链表
+	return newHead
+}
