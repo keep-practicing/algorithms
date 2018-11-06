bcc3a4fd3f23605a4757a28fb1343fe096b05c82
zwfang <zwfangio@hotmail.com>
2018-11-06 16:54:01 +0800

    add more test case for valid parentheses

--- leetcode/0020_valid_parentheses/valid_parentheses_test.go
+++ leetcode/0020_valid_parentheses/valid_parentheses_test.go
@@ -3,8 +3,8 @@ package validparentheses
 import "testing"
 
 func TestIsValid(t *testing.T) {
-	testData := []string{"()", "(((((())))))", "()()()()", "(((((((()", "((()(())))"}
-	expectedData := []bool{true, true, true, false, true}
+	testData := []string{"()", "(((((())))))", "()()()()", "(((((((()", "((()(())))", "", ")(", "}{", "][", "(][)"}
+	expectedData := []bool{true, true, true, false, true, true, false, false, false, false}
 
 	for i, s := range testData {
 		result := isValid(s)
