dd2da5a14c74747a96f4a889b5f544091a54aaa2
zwfang <zwfangio@hotmail.com>
2018-11-09 08:28:32 +0800

    add more ut case

--- leetcode/0021_merge_two_sorted_lists/mergeTwoLists_test.go
+++ leetcode/0021_merge_two_sorted_lists/mergeTwoLists_test.go
@@ -6,37 +6,63 @@ import (
 )
 
 func TestMergeTwoLists(t *testing.T) {
-	testData := struct {
+	testDatas := []struct {
 		name     string
 		agr1     *ListNode
 		arg2     *ListNode
 		expected *ListNode
 	}{
-		name: "one",
-		agr1: &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5, Next: nil}}},
-		arg2: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 6, Next: nil}}},
-		expected: &ListNode{
-			Val: 1, Next: &ListNode{
-				Val: 2, Next: &ListNode{
-					Val: 3,
-					Next: &ListNode{
-						Val: 4,
+		{
+			name: "one",
+			agr1: &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5, Next: nil}}},
+			arg2: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 6, Next: nil}}},
+			expected: &ListNode{
+				Val: 1, Next: &ListNode{
+					Val: 2, Next: &ListNode{
+						Val: 3,
 						Next: &ListNode{
-							Val: 5,
+							Val: 4,
 							Next: &ListNode{
-								Val:  6,
-								Next: nil,
+								Val: 5,
+								Next: &ListNode{
+									Val:  6,
+									Next: nil,
+								},
 							},
 						},
 					},
 				},
 			},
 		},
+		{
+			name: "two",
+			agr1: &ListNode{Val: 1, Next: nil},
+			arg2: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 6, Next: nil}}},
+			expected: &ListNode{
+				Val: 1, Next: &ListNode{
+					Val: 2, Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val:  6,
+							Next: nil,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:     "three",
+			agr1:     nil,
+			arg2:     nil,
+			expected: nil,
+		},
 	}
 
-	t.Run(testData.name, func(t *testing.T) {
-		if result := mergeTwoLists(testData.agr1, testData.arg2); !reflect.DeepEqual(result, testData.expected) {
-			t.Errorf("mergeTwoLists() = %v, expected %v", result, testData.expected)
-		}
-	})
+	for _, testData := range testDatas {
+		t.Run(testData.name, func(t *testing.T) {
+			if result := mergeTwoLists(testData.agr1, testData.arg2); !reflect.DeepEqual(result, testData.expected) {
+				t.Errorf("mergeTwoLists() = %v, expected %v", result, testData.expected)
+			}
+		})
+	}
 }
