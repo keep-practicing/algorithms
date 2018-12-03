dc8d636bab38bbdbc2890bd3a6d33e569b9053ca
zwfang <zwfangio@hotmail.com>
2018-12-03 13:29:35 +0800

    optimize heap sort test

--- essay/sort/heap_test.go
+++ essay/sort/heap_test.go
@@ -1,12 +1,20 @@
 package sort
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestHeapSort(t *testing.T) {
-	arr := []int{55, 34, 22, 67, 2, 3, 4, 1}
-	HeapSort(arr, 8)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	var arr []int
+	for i := 0; i < 9; i++ {
+		arr = append(arr, r.Intn(100))
+	}
+
+	HeapSort(arr, len(arr))
 	for i := range arr {
 		if i+1 < len(arr) && arr[i] > arr[i+1] {
 			t.Error("Failed, HeapSort.")
