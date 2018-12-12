21a6ea8745f60f63debd1c34096bd69eca0a6d15
Zhaowei <zwfangio@hotmail.com>
2018-12-12 17:18:42 +0800

    add more ut and practice defer panic recover

--- algorithm/dp/knapsack_problem01/knapsack_problem01_test.go
+++ algorithm/dp/knapsack_problem01/knapsack_problem01_test.go
@@ -14,13 +14,31 @@ func TestKnapsackProblem01(t *testing.T) {
 			v: []int{6, 10, 12},
 			c: 5,
 		},
+		Knapsack{
+			w: []int{},
+			v: []int{},
+			c: 5,
+		},
 	}
 
-	expectedData := []int{22}
+	expectedData := []int{22, 0}
 
 	for index, data := range testData {
 		if res := knapsack01(data.w, data.v, data.c); res != expectedData[index] {
 			t.Errorf("expected %d, got %d", expectedData[index], res)
 		}
 	}
+
+	knapsack := Knapsack{
+		w: []int{1, 2, 3, 3},
+		v: []int{6, 10, 12},
+		c: 5,
+	}
+
+	defer func() {
+		if err := recover(); err == nil {
+			t.Error("Failed, panic error can't catch")
+		}
+	}()
+	knapsack01(knapsack.w, knapsack.v, knapsack.c)
 }
