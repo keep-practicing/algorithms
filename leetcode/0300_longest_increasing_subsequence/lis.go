e120f1369b2ada5e7beb8da2147e1b0388c7be04
Zhaowei <zwfangio@hotmail.com>
2018-12-13 15:58:56 +0800

    leetcode LIS

---
+++ leetcode/0300_longest_increasing_subsequence/lis.go
@@ -0,0 +1,26 @@
+package lis
+
+import "op/utils"
+
+func lengthOfLIS(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+
+	memo := make([]int, n)
+	for i := 0; i < n; i++ {
+		memo[i] = 1
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				memo[i], _ = utils.CalcMaxInt(memo[i], 1+memo[j])
+			}
+		}
+	}
+
+	res, _ := utils.CalcMaxInt(memo...)
+	return res
+}
--- test.sh
+++
@@ -1,6 +0,0 @@
-function test() {
-    go clean -testcache
-    go test ./...
-}
-
-test
