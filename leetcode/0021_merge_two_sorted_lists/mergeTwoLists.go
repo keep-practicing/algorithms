bffa48e81513fa637d8d8b9e2a0f2055482b7928
Zhaowei <zwfangio@hotmail.com>
2018-12-18 12:31:17 +0800

    optimize code

--- leetcode/0021_merge_two_sorted_lists/mergeTwoLists.go
+++ leetcode/0021_merge_two_sorted_lists/mergeTwoLists.go
@@ -1,13 +1,13 @@
 /*
-	21. Merge Two Sorted Lists
-	Source: https://leetcode.com/problems/merge-two-sorted-lists/
+21. Merge Two Sorted Lists
+Source: https://leetcode.com/problems/merge-two-sorted-lists/
 
-	Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
-	Example:
+Example:
 
-	Input: 1->2->4, 1->3->4
-	Output: 1->1->2->3->4->4
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
 */
 
 package mergetwolists
